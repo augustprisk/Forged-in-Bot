0844b325e15d5e32c3586122970712d1a129c802
augustprisk <guss.prisk@gmail.com>
2026-02-05 22:23:25 +1300

    Refactor result files to include winner of episode and get rid of file noise

--- commands/forged_in_fire/result.js
+++ commands/forged_in_fire/result.js
@@ -5,71 +5,33 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName('result')
   .setDescription('Was your guess right or wrong?')
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('win')
-      .setDescription('Your guess was correct!!!')
-      .addStringOption((Option) => 
-        Option
-            .setName('user')
-            .setDescription('August or Grace?')
-            .setRequired(true)
-            .addChoices(
-                { name: 'August', value: 'August' },
-                { name: 'Grace', value: 'Grace' }
-            )
+  .addStringOption((Option) => 
+    Option
+        .setName('user')
+        .setDescription('August or Grace?')
+        .setRequired(true)
+        .addChoices(
+            { name: 'August', value: 'August' },
+            { name: 'Grace', value: 'Grace' }
+        )
       )
-      .addStringOption((option) => option.setName('weapon').setDescription('What was the final weapon?').setRequired(true))
-  )
-  .addSubcommand((subcommand) => 
-    subcommand
-      .setName('loss')
-      .setDescription('Your guess was wrong!!!')
-      .addStringOption((Option) => 
-        Option
-            .setName('user')
-            .setDescription('August or Grace?')
-            .setRequired(true)
-            .addChoices(
-                { name: 'August', value: 'August' },
-                { name: 'Grace', value: 'Grace' }
-            )
-      )
-      .addStringOption((option) => option.setName('weapon').setDescription('What was the final weapon?').setRequired(true))
-  ),
+      .addStringOption((option) => option.setName('winner').setDescription('who won?').setRequired(true))
+      .addStringOption((option) => option.setName('weapon').setDescription('What was the final weapon?').setRequired(true)),
   async execute(interaction) {
-    if (interaction.options.getSubcommand() === 'win') {
-      const result = 'win';
-      const userName = interaction.options.getString('user', true);
-      const weapon = interaction.options.getString('weapon', true);
-      const point = 1;
+    const userName = interaction.options.getString('user', true);
+    const winner = interaction.options.getString('winner', true);
+    const weapon = interaction.options.getString('weapon', true);
+    const point = 1;
 
-      try {
-        writeResult(userName, result, weapon, point)
-        await interaction.reply(
-          `${interaction.user.username}, your win has been recorded`
-        )
-      } catch(err) {
-        await interaction.reply(
-          `There was an error with this command: ${err}`
-        )
-      }
-    } else if (interaction.options.getSubcommand() === 'loss') {
-      const result = 'lose';
-      const userName = interaction.options.getString('user', true);
-      const weapon = interaction.options.getString('weapon', true);
-      const point = 0
-
-      try {
-        writeResult(userName, result, weapon, point)
-        await interaction.reply(
-          `${interaction.user.username}, your loss has been recorded`
-        )
-      } catch(err) {
-        await interaction.reply(
-          `There was an error with this command: ${err}`
-        )
-      }
+    try {
+      writeResult(userName, winner, weapon, point)
+      await interaction.reply(
+        `${userName}, your result has been recorded`
+      )
+    } catch(err) {
+      await interaction.reply(
+        `There was an error with this command: ${err}`
+      )
     }
   }
 }
